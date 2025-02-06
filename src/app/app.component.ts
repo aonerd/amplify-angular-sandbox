9d6b118ee750fece858bf4345868d448d54b46e7
Aiden O'Neill <aiden@oneillsoftware.com>
2025-02-06 22:33:47 +0000

    Fixing locks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AmplifyAuthenticatorModule, AuthenticatorService} from '@aws-amplify/ui-angular';
+import {Amplify} from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'amplify-angular-sandbox';
+
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(outputs);
+  }
 }
